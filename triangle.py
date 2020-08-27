1e3a8f2500c2a6e45cfd367d1045eafe2b455312
Singh <ssingh17@una.edu>
2020-08-27 16:43:22 -0500

    First Lecture

--- triangle.py
+++ triangle.py
@@ -4,7 +4,18 @@
 #Date:- 8/27/2020
 import math
 print("Compute the hypotenuse of right triangle")
-leg1 = float(input("Enter first length:"))
-leg2 = float(input("Enter second length:"))
-leg3=math.sqrt(leg1*leg1+leg2*leg2)
-print("Hypotenuse is ",round(leg3,2))
+
+
+again="Y"
+
+while again == "Y" :
+    leg1 = float(input("Enter first length:"))
+    leg2 = float(input("Enter second length:"))
+    if leg1<=0:
+        print("Leg #1 must be positive")
+    elif leg2<=0:
+        print("Leg #2 must be positive")
+    else:
+        leg3=math.sqrt(leg1*leg1+leg2*leg2)
+        print("Hypotenuse is ",round(leg3,2))
+    again = input("Go again(Y/N)?")
