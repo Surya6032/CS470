1da4e02dc938d5f837a7738b5ce9674dad193616
Singh <ssingh17@una.edu>
2020-08-27 16:30:59 -0500

    First Lecture

--- triangle.py
+++ triangle.py
@@ -4,7 +4,7 @@
 #Date:- 8/27/2020
 import math
 print("Compute the hypotenuse of right triangle")
-leg1 = int(input("Enter first length:"))
-leg2 = int(input("Enter second length:"))
+leg1 = float(input("Enter first length:"))
+leg2 = float(input("Enter second length:"))
 leg3=math.sqrt(leg1*leg1+leg2*leg2)
 print(leg3)
