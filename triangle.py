756a39fa06283d792ea5091aa146c8a68c04a741
Singh <ssingh17@una.edu>
2020-08-27 16:30:16 -0500

    First Lecture

---
+++ triangle.py
@@ -0,0 +1,10 @@
+#Program to print hello world
+#Written by Surya Singh
+#CS 470
+#Date:- 8/27/2020
+import math
+print("Compute the hypotenuse of right triangle")
+leg1 = int(input("Enter first length:"))
+leg2 = int(input("Enter second length:"))
+leg3=math.sqrt(leg1*leg1+leg2*leg2)
+print(leg3)
