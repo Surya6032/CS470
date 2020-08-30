eba96637a6dabdba9e010413fb597e52b5a49780
Surya Singh <psurya.924@gmail.com>
2020-08-30 18:28:31 -0500

    Create second.py

---
+++ second.py
@@ -0,0 +1,11 @@
+value=int(input("Enter max number:"))
+numbers=[True for i in range(value)]
+for i in range(len(numbers)): 
+    for j in range(2,i): 
+        if(i % j==0): 
+            break
+    else: 
+        numbers[i]=False
+for i in range(len(numbers)):
+  if numbers[i]==False:
+    print(i)
